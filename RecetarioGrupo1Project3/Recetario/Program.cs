2b4a92efa741dbaa16ad4a51ae6e7c0ac4db433c
AxelGenao <65257294+AxelGenao@users.noreply.github.com>
2022-11-29 21:00:48 -0400

    Add files via upload

---
+++ RecetarioGrupo1Project3/Recetario/Program.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+
+var input = CharStreams.fromPath(@"..\..\..\Recetas.txt");
+
+Console.WriteLine("Los SQL INSERT son:");
+
+var lexer = new RecetarioLexer(input);
+var tokenStream = new CommonTokenStream(lexer);
+var parser = new RecetarioParser(tokenStream);
+var tree = parser.recetario();
+var recetario = new Recetario.Recetario();
+recetario.Visit(tree);
+
+int n = 0;
+foreach (string line in recetario.Output)
+{
+    n++;
+    Console.WriteLine(line);
+}
+Console.WriteLine("\nDele a alguna tecla para salir.");
+Console.ReadKey();
